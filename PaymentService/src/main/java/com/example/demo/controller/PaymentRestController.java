565258e912b6a516bff151e0d91e1493c518cc85
Saravanan Chinnadurai <saranchinnadurai@gmail.com>
2022-02-01 23:27:39 +0800

    Adding payment service components

---
+++ PaymentService/src/main/java/com/example/demo/controller/PaymentRestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentRestController {
+	@GetMapping("/validate")
+	public ResponseEntity<String> validatePayment() {
+		System.out.println("Payment Page");
+		return ResponseEntity.ok("PAYMENT SUCCESS!");
+	}
+}
